c3589174df112df50fb443a61cf2af7e0379dd7a
MMMVM <42170207+MMMVM@users.noreply.github.com>
2019-06-07 10:18:41 +0530

    Update armstrongnum.py

--- armstrongnum.py
+++ armstrongnum.py
@@ -1,11 +1,11 @@
-number = int(input())
+b = int(input())
 sum = 0
-temp = number
+temp = b
 while temp > 0:
    digit = temp % 10
    sum += digit ** 3
    temp //= 10
-if number == sum:
+if b== sum:
    print("yes")
 else:
    print("no")
